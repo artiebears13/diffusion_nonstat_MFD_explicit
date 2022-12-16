eacd0933f3761c7c086f39e6c96fdd84c8edc975
ArtMed <artiebears@mail.ru>
2022-12-16 23:18:43 +0300

    speed up algo by reindexing

--- CPP/main2.cpp
+++ CPP/main2.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <cmath>
 #include <chrono>
+#include <array>
 #include <omp.h>
 
-#define Nx 21
-#define Ny 21
-#define Nz 21
+
+#define Nx 120
+#define Ny 60
+#define Nz 20
 #define n Nx
 #define m Ny
 #define kk Nz
+#define len n * m * kk
 #define hx 1./(n-1)*1.0
 #define hy 1. / (m - 1) * 1.0
 #define hz 1. / (kk - 1) * 1.0
@@ -23,78 +26,74 @@
 
 
 inline double get_u(double *const &u, int i, int j, int k) {
-    return *(u+k * n * m + j * n + i);
+    return *(u + k * n * m + j * n + i);
 }
 
-inline void set_u(double* const &u, int i, int j, int k, double value) {
+inline void set_u(double *const &u, int i, int j, int k, double const &value) {
     *(u + k * n * m + j * n + i) = value;
 }
 
-inline void set_value_next(double *u_next, int i, int j, int k, const double value) {
-    *(u_next + i * n * m + j * kk + k) = value;
-}
 
-inline void fill_boundary_x(double * u_actual) {
-    for (int j = 0; j < m; ++j) {
-        for (int k = 0; k < kk; ++k) {
-            set_u(u_actual, 0, j, k, 0.);
-            set_u(u_actual, n - 1, j, k, 0.);
-        }
-    }
-}
-
-inline void fill_boundary_y(double *const &u_actual) {
-    for (int i = 0; i < n; ++i) {
-        for (int k = 0; k < kk; ++k) {
-            set_u(u_actual, i, 0, k, 0.);
-            set_u(u_actual, i, m - 1, k, 0.);
-        }
-    }
-}
-
-
-void fill_boundary_z(double *&u_actual) {
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            set_u(u_actual, i, j, 0, 0.);
-            set_u(u_actual, i, j, kk - 1, 0.);
-        }
-    }
-}
-
-inline double Lx(double * const &u_actual, int i, int j, int k) {
-    return (get_u(u_actual, i - 1, j, k) - 2 * get_u(u_actual, i, j, k) + get_u(u_actual, i + 1, j, k))
+inline double Lx(double *const &u_actual, int i, int j, int k) {
+//    std::cout<<"1 "<<(i + 1) + j + k<<std::endl;
+    return (*(u_actual + (i - 1) + j + k) - 2 * (*(u_actual + (i) + (j) + k)) +
+            *(u_actual + (i +1) + j + k))
            / (hx * hx);
 }
 
-inline double Ly(double * const &u_actual, int i, int j, int k) {
-    return (get_u(u_actual, i, j - 1, k) - 2 * get_u(u_actual, i, j, k) + get_u(u_actual, i, j + 1, k))
+inline double Ly(double *const &u_actual, int i, int j, int k) {
+//    std::cout<<"1 "<<(i) + (j + n) + k<<std::endl;
+    return (*(u_actual + (i) + (j - n) + k) - 2 * (*(u_actual + (i) + (j) + k)) +
+    *(u_actual + (i) + (j + n) + k))
            / (hy * hy);
 }
 
-inline double Lz(double * const &u_actual, int i, int j, int k) {
-    return (get_u(u_actual, i, j, k - 1) - 2 * get_u(u_actual, i, j, k) + get_u(u_actual, i, j, k + 1))
+inline double Lz(double *const &u_actual, int i, int j, int k) {
+//    std::cout<<"1 "<<(i) + (j) + k + n*m<<std::endl;
+    return ((*(u_actual + (i) + (j) + k - n*m)) - 2 * (*(u_actual + (i) + (j) + k)) +
+    (*(u_actual + (i) + (j) + k + n*m)))
            / ((hz) * (hz));
 }
 
 void swap(double *&u_actual, double *&u_next) {
-    auto tmp = u_actual;
+
+
+    double *temp = u_actual;
     u_actual = u_next;
-    u_next = tmp;
+    u_next = temp;
+
 }
 
 int main() {
-    int len = n * m * kk;
-    double *u_actual = new double[len];
-    double *u_next = new double[len];
-    for (int i = 1; i < n; ++i) {
-        for (int j = 1; j < m; ++j) {
-            for (int k = 1; k < kk; ++k) {
+
+    double u;
+    double u_act[len];
+    double u_n[len];
+
+    double *u_actual = &u_act[0];
+    double *u_next = &u_n[0];
+
+
+//    double* u_actual=u_act.data();
+//    double *u_next=u_n.data();
+
+    for (int k = 0; k < kk; ++k) {
+        for (int j = 0; j < m; ++j) {
+            for (int i = 0; i < n; ++i) {
+
                 set_u(u_actual, i, j, k, 0.);
-//                set_u(u_next,i,j,k,0.);
+
             }
         }
     }
+
+//    std::cout<< get_u(u_actual,5,5,5)<<std::endl;
+
+//    double *u_actual = u_act.();
+//    double *u_next = u_n.data();
+
+
+
 
 //    set_u(u_actual, 1, 0, 0, 10.);
 //    set_u(u_next, 1, 0, 0, 0.);
@@ -102,46 +101,52 @@ int main() {
 //    swap(u_actual, u_next);
 //    std::cout << get_u(u_next, 1, 0, 0) << std::endl;
 //    std::cout << get_u(u_actual, 1, 0, 0) << std::endl;
-
+//-----------------------------------------------------------------------------
     clock_t tStart = clock();
 //   auto t = omp_get_wtime();
     for (int l = 1; l * dt <= 1; l++) {
 //        print();
-//        std::cout<<i<<std::endl;
-        double u;
-        for (int k = 0; k < kk; ++k) {
-//#pragma omp parallel for
-            for (int j = 0; j < m; ++j) {
-                for (int i = 0; i < n; ++i) {
-                    if (i == 0) { set_u(u_actual, 0, j, k, 0.); }
-                    else if (i == n - 1) { set_u(u_actual, i, j, k, 0.); }
-                    else if (j == 0) { set_u(u_actual, i, 0, k, 0.); }
-                    else if (j == m - 1) { set_u(u_actual, i, j, k, 0.); }
+//        std::cout << "-------------------------------------------" << std::endl;
 
-                    else if (k == 0) { set_u(u_actual, i, j, 0, 0.); }
-                    else if (k == kk - 1) { set_u(u_actual, i, j, k, 0.); }
+        for (int k = 0; k < n * m * kk; k += n * m) {
+//#pragma omp parallel for
+            for (int j = 0; j < m * n; j += n) {
+                for (int i = 0; i < n; ++i) {
+//                    std::cout << " i=" << i << " j=" << j << " k=" << k << std::endl;
+                    if (i == 0) { *(u_next + i + j + k) = 0.; }
+                    else if (i == n - 1) { *(u_next + i + j + k) = 0.; }
+                    else if (j == 0) { *(u_next + i + j + k) = 0.; }
+                    else if (j == m * n - n) { *(u_next + i + j + k) = 0.; }
+
+                    else if (k == 0) { *(u_next + i + j + k) = 0.; }
+                    else if (k == n * m * kk - m*n) { *(u_next + i + j + k) = 0.; }
                     else {
-                        u = get_u(u_actual, i, j, k) +
-                            dt * (f(i * hx, j * hy, k * hz)
+                        u = *(u_actual + i + j + k) +
+                            dt * (f(i  * hx, j / n * hy, k /n/m* hz)
                                   + D0 * Lx(u_actual, i, j, k)
                                   + D1 * Ly(u_actual, i, j, k)
                                   + D2 * Lz(u_actual, i, j, k));
-//                    std::cout << "dt: " << dt << " get_u: " << get_u(u_actual, i, j, k) << " " << " f(i, j, k): "
-//                              << f(i * hx, j * hy, k * hz) <<
-//                              " D0 * Lx(i, j, k): " << +D0 * Lx(u_actual, i, j, k) << " D1 * Ly(i, j, k): "
-//                              << D1 * Ly(u_actual, i, j, k)
-//                              << " D2 * Lz(i, j, k): " << D2 * Lz(u_actual, i, j, k) << " u: "
-//                              << u << std::endl;
+//                        std::cout << "dt: " << dt << " get_u: " << get_u(u_actual, i, j, k) << " " << " f(i, j, k): "
+//                                  << f(i * hx, j * hy, k * hz) <<
+//                                  " D0 * Lx(i, j, k): " << D0 * Lx(u_actual, i, j, k) << " D1 * Ly(i, j, k): "
+//                                  << D1 * Ly(u_actual, i, j, k)
+//                                  << " D2 * Lz(i, j, k): " << D2 * Lz(u_actual, i, j, k) << " u: "
+//                                  << u << std::endl;
 
-                        set_u(u_next, i, j, k, u);
+//                        std::cout << "i: " << k + j + i << "   " << *(u_actual + k + j + i) << std::endl;
+
+                        *(u_next + i + j + k) = u;
                     }
                 }
             }
         }
+//        std::cout << "BEFORE: " << "u_actu: " << u_actual[93] << " u_next: " << u_next[93] << std::endl;
+
         swap(u_actual, u_next);
+//        std::cout << " AFTER: " << "u_actu: " << u_actual[93] << " u_next: " << u_next[93] << std::endl;
     }
 
-    auto time = (double) (clock() - tStart) / CLOCKS_PER_SEC;
+    double time = (double) (clock() - tStart) / CLOCKS_PER_SEC;
 //   t=omp_get_wtime() - t;
     std::cout << "time: " << time << std::endl;
 
@@ -150,7 +155,11 @@ int main() {
         for (int j = 0; j < m; ++j) {
             for (int i = 0; i < n; ++i) {
 //                std::cout <<"mist: "<< mist << std::endl;
-//                std::cout<<"numbering: "<< get_u(u_actual, i, j, k)<<" analytic: " <<get_analytic_u(i * hx, j * hy, k * hz)<<std::endl;
+
+
+
+//                std::cout<<"numbering: "<< get_u(u_actual, i, j, k)<<" analytic: " <<get_analytic_u(i * hx, j * hy, k * hx)<<std::endl;
+
                 if (std::abs(get_u(u_actual, i, j, k) - get_analytic_u(i * hx, j * hy, k * hz)) > mist) {
                     mist = std::abs(get_u(u_actual, i, j, k) - get_analytic_u(i * hx, j * hy, k * hz));
                 }
