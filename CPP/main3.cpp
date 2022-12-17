6bc5b02b50b766b3f755555a3c4e007e5817ae79
ArtMed <artiebears@mail.ru>
2022-12-17 18:43:58 +0300

    added plotting

--- CPP/main3.cpp
+++ CPP/main3.cpp
@@ -1,35 +1,60 @@
 #include <iostream>
 #include <cmath>
 #include <chrono>
+#include <fstream>
+#include <cstring>
 #include <array>
 #include <omp.h>
+#include "to_vtk.hpp"
 
 
-#define Nx 120
-#define Ny 60
-#define Nz 20
-#define n Nx
-#define m Ny
-#define kk Nz
-#define len n * m * kk
-#define hx 1./(n-1)*1.0
-#define hy 1. / (m - 1) * 1.0
-#define hz 1. / (kk - 1) * 1.0
-#define D0 0.25
-#define D1 0.15
-#define D2 0.1
-#define dt 0.9*(0.5 /(D0 / (hx*hx) + D1 / (hy*hy) + D2 / (hz*hz)))
+//#define Nx 120
+//#define Ny 60
+//#define Nz 20
+//#define n Nx
+//#define m Ny
+//#define kk Nz
+//#define len n * m * kk
+//#define hx 1./(n-1)*1.0
+//#define hy 1. / (m - 1) * 1.0
+//#define hz 1. / (kk - 1) * 1.0
+//#define D0 0.25
+//#define D1 0.15
+//#define D2 0.1
+//#define dt 0.9*(0.5 /(D0 / (hx*hx) + D1 / (hy*hy) + D2 / (hz*hz)))
 //#define dt (0.45 /(D0 / (hx*hx) + D1 / (hy*hy) + D2 / (hz*hz)))
 //#define dt 0.001
+
 #define get_analytic_u(x, y, z) (sin(M_PI * x) * sin(M_PI * y) * sin(M_PI * z) *(1 - exp(-((D0 + D1 + D2) * M_PI*M_PI * 1))))
-#define f(x, y, z) ((D0+D1+D2)*(M_PI*M_PI*sin(M_PI*x))* sin(M_PI * y) * sin(M_PI * z))
+//#define f(x, y, z) ((D0+D1+D2)*(M_PI*M_PI*sin(M_PI*x))* sin(M_PI * y) * sin(M_PI * z))
+
+const unsigned Nx=122;
+const unsigned Ny=62;
+const unsigned Nz=22;
+const unsigned n =Nx;
+const unsigned m =Ny;
+const unsigned kk= Nz;
+const unsigned len= n * m * kk;
+const double hx= 1./(n-1)*1.0;
+const double hy= 1. / (m - 1) * 1.0;
+const double hz= 1. / (kk - 1) * 1.0;
+const double D0= 0.25;
+const double D1= 0.15;
+const double D2= 0.1;
+const double dt=(0.45 /(D0 / (hx*hx) + D1 / (hy*hy) + D2 / (hz*hz)));
+
+
+
+
+
+inline double f(double x, double y, double z) {return((D0+D1+D2)*(M_PI*M_PI*sin(M_PI*x))* sin(M_PI * y) * sin(M_PI * z));}
 
 
 inline double get_u(double *const &u, int i, int j, int k) {
     return *(u + k * n * m + j * n + i);
 }
 
-inline void set_u(double * const &u, int i, int j, int k, double const &value) {
+inline void set_u(double *const &u, int i, int j, int k, double const &value) {
     *(u + k * n * m + j * n + i) = value;
 }
 
@@ -49,7 +74,7 @@ inline double Lz(double *const &u_actual, int i, int j, int k) {
            / ((hz) * (hz));
 }
 
-void swap(double * &u_actual, double * &u_next) {
+inline void swap(double *&u_actual, double *&u_next) {
 
 
     double *temp = u_actual;
@@ -101,11 +126,10 @@ int main() {
     for (int l = 1; l * dt <= 1; l++) {
 //        print();
 //        std::cout<<i<<std::endl;
-        for (int i = 0; i < n; ++i)
-         {
+        for (unsigned k = 0; k < kk; ++k) {
 //#pragma omp parallel for
-            for (int j = 0; j < m; ++j) {
-                for (int k = 0; k < kk; ++k){
+            for (unsigned j = 0; j < m; ++j) {
+                for (unsigned i = 0; i < n; ++i) {
                     if (i == 0) { set_u(u_next, i, j, k, 0.); }
                     else if (i == n - 1) { set_u(u_next, i, j, k, 0.); }
                     else if (j == 0) { set_u(u_next, i, j, k, 0.); }
@@ -159,6 +183,7 @@ int main() {
             }
         }
     }
+    write_to_file(u_actual,n,m,kk);
 
     std::cout << "mist: " << mist << std::endl;
     std::cout << "dt: " << dt << std::endl;
